7edff0a57146dd4835bd473bcd1412a27edacf65
Vince <vincivenv@gmail.com>
2023-03-19 06:57:20 +0100

    Switch to TcpListener

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
+
 use rusapi::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed binding to port specified");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
     // the health check is exposed at /health_check;
@@ -5,10 +7,10 @@ async fn health_check_works() {
     // the health check always returns a 200;
     // the health check’s response has no body
 
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(format!("{}/health_check", address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -18,7 +20,12 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = rusapi::run().expect("Failed to start bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    let port = listener.local_addr().unwrap().port();
+
+    let server = rusapi::run(listener).expect("Failed to start bind address");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }

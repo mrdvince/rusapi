9447ff8df5302dc5836609fc3dc732adb2487545
Vince <vincivenv@gmail.com>
2023-03-20 16:11:32 +0100

    subscriber tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -29,3 +29,45 @@ fn spawn_app() -> String {
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
+
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form() {
+    let app_address = spawn_app();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let client = reqwest::Client::new();
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to send request");
+    assert_eq!(200, response.status().as_u16())
+}
+
+#[tokio::test]
+async fn subscriber_returns_404_for_invalid_form() {
+    let app_address = spawn_app();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+    let client = reqwest::Client::new();
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to send form data");
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        )
+    }
+}

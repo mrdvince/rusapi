47de7961bc22d976996c1c3a0991912e42d47333
Vince <vincivenv@gmail.com>
2023-03-27 23:26:04 +0200

    Placeholder config file

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,1 +1,23 @@
+use config::{Config, ConfigError};
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
 
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+// Read config from configuration file
+pub fn get_configuration() -> Result<Settings, ConfigError> {
+    let settings = Config::builder()
+        .add_source(config::File::with_name("config.yaml"))
+        .build()?;
+    settings.try_deserialize()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::net::TcpListener;
 
-use rusapi::startup::run;
+use rusapi::{configuration::get_configuration, startup};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed binding to port specified");
-    run(listener)?.await
+    let configuration = get_configuration().expect("Failed to read configurartion file");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed binding to port specified");
+
+    startup::run(listener)?.await
 }
